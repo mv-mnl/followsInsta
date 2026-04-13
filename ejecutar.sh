c37b0d6d5f9916c960fd88936a336ea8ee871475
mv-mnl <mmelendez1505@gmail.com>
2026-04-13 09:25:47 -0600

    fix: ejectanto todo desde un mismo .sh

--- ejecutar.sh
+++ ejecutar.sh
@@ -4,3 +4,10 @@ python3 analyze_instagram.py
 sleep 5
 python3 open_profiles.py
 
+
+sleep 5
+shopt -s extglob
+rm -rf !(.git|.gitignore|analyze_instagram.py|open_profiles.py|README.md|*.sh) 
+shopt -u extglob 
+echo "Limpieza completada: Solo quedan tus archivos de código y configuración."
+
